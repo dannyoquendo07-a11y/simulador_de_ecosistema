6c4e6b650fd700f762ab419af37d50e825feb3a7
Miguel Martinez <martinezpmg9@gmail.com>
2026-05-13 19:46:05 +0000

    diseño de tests???

---
+++ tests.py
@@ -0,0 +1,20 @@
+import unittest
+
+class EcosistemaTests(unittest.TestCase):
+
+    def test_comer_vegetacion(self):
+        # que un herviboro pueda comer vegetacion
+
+        # crear un herviboro
+
+        # crear la vegetacion
+
+        # probar la interaccion
+    def test_carnivoro_vegetacion(self):
+        #
+        #
+        #
+        #
+        self.assertFalse
+
+        
