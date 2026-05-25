7180cb20051c5a1391b18e40665088bffb613779
dannyoquendo07@gmail.com <dannyoquendo07@gmail.com>
2026-05-24 20:40:05 -0400

    Primer test de poblacion inicial

--- tests.py
+++ tests.py
@@ -1,4 +1,10 @@
 import unittest
+from molde import Especies
+
+class TestEspecies(unittest.TestCase):
+    def test_inicializacion_poblacion(self):
+        lobo=Especies("Lobo",50,0.1,0.05)
+        self.assertEqual(lobo.poblacion,50)
 
 
 class EcosistemaTests(unittest.TestCase):
@@ -19,3 +25,5 @@ class EcosistemaTests(unittest.TestCase):
         #
         #
         ...
+if __name__ == "__main__":
+    unittest.main()
