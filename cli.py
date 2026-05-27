f4c97e976bcdc8c1142eb83898418102ae8edb48
Miguel Martinez <martinezpmg9@gmail.com>
2026-05-27 19:51:13 +0000

    observaciones

---
+++ cli.py
@@ -0,0 +1,4 @@
+input("pulsa no se que para hacer no se que ")
+
+if x = 1:
+    
---
+++ gui.py
@@ -0,0 +1,15 @@
+import tkinter
+
+app = tkkking
+
+
+asdmas
+assertdfas
+finallyasd
+async def async def assert
+asda(parameter_list):
+    pass(parameter_list):
+    pass
+
+    # si pulso este boton va a pasar algo
+        
