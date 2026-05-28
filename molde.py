d9c5b69a39d034ac2180edad89b4cbc7a8a89d79
dannyoquendo07@gmail.com <dannyoquendo07@gmail.com>
2026-05-28 17:52:23 -0400

    Corregi la clase entorno a la principal

--- molde.py
+++ molde.py
@@ -2,68 +2,6 @@ from dataclasses import dataclass
 import random
 
 @dataclass
-# todo: cambiar a especia
-class Especie:
-    nombre:str
-    poblacion_inicial:int
-    tasa_reproduccion:float
-    tasa_mortalidad:float
-    
-    def __post_init__(self):
-        self._poblacion=self.poblacion_inicial
-
-    @property
-    def poblacion(self)->int:
-        return self._poblacion
-    @poblacion.setter
-    def poblacion(self,valor:int):
-        if valor<0:
-            print(f"La especie: '{self.nombre}' se ha extinguido")
-            self._poblacion=valor
-
-    def actualizar(self,recursos_disponibles:float):
-        nacimientos=self.poblacion*self.tasa_reproduccion*(recursos_disponibles/100)
-        muertes=self.poblacion*self.tasa_mortalidad
-        self.poblacion+=nacimientos-muertes
-        
-        if self.poblacion<0:
-            self.poblacion=0
-        self.poblacion=int(self.poblacion)
-
-@dataclass
-class Depredador(Especies):
-    eficacia_caza:float
-    hambre:float
-
-    def cazar(self,presa):
-        if presa.poblacion>0:
-            capturas=self.poblacion*self.eficacia_caza
-            if capturas>presa.poblacion:
-                capturas=presa.poblacion
-            presa.poblacion-=int(capturas)
-            return capturas
-        return 0
-    
-    def comer(self,presas_capturadas:int):
-        self.hambre-=presas_capturadas
-
-@dataclass
-class Presa(Especies):
-    resistencia:float
-    camuflaje:float
-    
-    def intentar_huir(self)->bool:
-        return random.random()<self.resistencia
-    
-    def comer(self,abundancia_recursos:float):
-        if abundancia_recursos>50:
-            self.poblacion+=int(self.poblacion*0.05)
-    
-    def buscar_refugio(self):
-        self.tasa_mortalidad*=0.8
-        print(f"La poblacion de: {self.nombre} se ha escondido")
-
-@dataclass
 class Entorno:
     vegetacion:float
     humedad:float
@@ -97,3 +35,65 @@ class Entorno:
     def avanzar_dia(self):
         self.dia_actual+=1   
         print(f"Dia: {self.dia_actual}")
+
+@dataclass
+
+class Especie:
+    nombre:str
+    poblacion_inicial:int
+    tasa_reproduccion:float
+    tasa_mortalidad:float
+    
+    def __post_init__(self):
+        self._poblacion=self.poblacion_inicial
+
+    @property
+    def poblacion(self)->int:
+        return self._poblacion
+    @poblacion.setter
+    def poblacion(self,valor:int):
+        if valor<0:
+            print(f"La especie: '{self.nombre}' se ha extinguido")
+            self._poblacion=valor
+
+    def actualizar(self,recursos_disponibles:float):
+        nacimientos=self.poblacion*self.tasa_reproduccion*(recursos_disponibles/100)
+        muertes=self.poblacion*self.tasa_mortalidad
+        self.poblacion+=nacimientos-muertes
+        
+        if self.poblacion<0:
+            self.poblacion=0
+        self.poblacion=int(self.poblacion)
+
+@dataclass
+class Depredador(Especie):
+    eficacia_caza:float
+    hambre:float
+
+    def cazar(self,presa):
+        if presa.poblacion>0:
+            capturas=self.poblacion*self.eficacia_caza
+            if capturas>presa.poblacion:
+                capturas=presa.poblacion
+            presa.poblacion-=int(capturas)
+            return capturas
+        return 0
+    
+    def comer(self,presas_capturadas:int):
+        self.hambre-=presas_capturadas
+
+@dataclass
+class Presa(Especie):
+    resistencia:float
+    camuflaje:float
+    
+    def intentar_huir(self)->bool:
+        return random.random()<self.resistencia
+    
+    def comer(self,abundancia_recursos:float):
+        if abundancia_recursos>50:
+            self.poblacion+=int(self.poblacion*0.05)
+    
+    def buscar_refugio(self):
+        self.tasa_mortalidad*=0.8
+        print(f"La poblacion de: {self.nombre} se ha escondido")
