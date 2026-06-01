2955f317c4d0a4d43276685b3341f2d287e2262d
dannyoquendo07@gmail.com <dannyoquendo07@gmail.com>
2026-06-01 19:12:36 -0400

    Pruebas de mortalidad en base a el hambre

--- tests.py
+++ tests.py
@@ -10,7 +10,22 @@ class TestEspecies(unittest.TestCase):
         conejo=Especie("Conejo",10,0.2,0.1)
         conejo.poblacion=-10
         self.assertEqual(conejo.poblacion, 0)
-
+    
+    def test_hambre_extrema_aumente_mortalidad(self):
+        """Verifica que si el hambre supera 70 la mortalidad aumente y mueran mas"""
+        #Especie con hambre normal '0'
+        ciervo_sano = Especie("Ciervo Sano", 100, tasa_reproduccion=0.0, tasa_mortalidad=0.1)
+        ciervo_sano.actualizar(recursos_disponibles=100)
+        #Muertes esperadas:100*0.1=10. Quedan 90
+        
+        #Especie con hambre extrema '80'
+        ciervo_hambriento = Especie("Ciervo Hambriento", 100, tasa_reproduccion=0.0, tasa_mortalidad=0.1)
+        ciervo_hambriento.hambre = 80.0
+        ciervo_hambriento.actualizar(recursos_disponibles=100)
+        #Muertes esperadas:100*(0.1+0.25)=35. Quedan 65
+        
+        self.assertEqual(ciervo_sano.poblacion, 90)
+        self.assertEqual(ciervo_hambriento.poblacion, 65)
 
 class EcosistemaTests(unittest.TestCase):
 
