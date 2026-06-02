d7a28a690fcc1ea19b69a80c31d6495603325828
dannyoquendo07@gmail.com <dannyoquendo07@gmail.com>
2026-06-02 00:43:19 -0400

    Primer ajuste en el main.py manejando las acciones diarias en un try-except

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
 from molde import Entorno, Presa, Depredador
+from excepciones import (ExcepcionDePoblacionInvalida,
+                         ExcepcionDeEspecieExtinta,
+                         ExcepcionDeRecursoInsuficiente,
+                         ExcepcionDeEstadoRefugioInvalido)
 
 class SimuladorEcosistema:
     def __init__(self,entorno:Entorno,presa:Presa,depredador:Depredador):
@@ -8,18 +12,26 @@ class SimuladorEcosistema:
     
     def ejecutar(self,dias:int):
         for avance in range(dias):
-            self.entorno.avanzar_dia()
-            
-            if self.presa.poblacion>0:
-                self.presa.comer(self.entorno.vegetacion)
-            
-            if self.depredador.poblacion > 0 and self.presa.poblacion > 0:
-                self.depredador.cazar(self.presa, self.entorno.clima)
-            
-            self.presa.actualizar(recursos_disponibles=self.entorno.vegetacion)
-            self.depredador.actualizar(recursos_disponibles=self.presa.poblacion)
+            try:
+                self.entorno.avanzar_dia()
+                #comportamiento de la presa
+                if self.entorno.clima=="Lluvia" and not self.presa.escondido:
+                    self.presa.buscar_refugio()
+                elif self.entorno.clims=="Soleado" and self.presa.escondido:
+                    self.presa.salir_de_refugio()
+                #alimentacion de las presas
+                if self.presa.poblacion>0:
+                    self.presa.comer(self.entorno.vegetacion)
+                #caza de deprepadores
+                if self.depredador.poblacion > 0 and self.presa.poblacion > 0:
+                    self.depredador.cazar(self.presa, self.entorno.clima)
+                #ciclo de vida
+                if self.presa.poblacion>0:
+                    self.presa.actualizar(recursos_disponibles=self.entorno.vegetacion)
+                if self.depredador.poblacion>0:
+                    self.depredador.actualizar(recursos_disponibles=self.presa.poblacion)
             print(f"Fin del Día {self.entorno.dia_actual} \n{self.presa.nombre}: {self.presa.poblacion} \n{self.depredador.nombre}: {self.depredador.poblacion}")
-
+            #evaluacion de colapso
             if self.presa.poblacion<=0 and self.depredador.poblacion<=0:
                 print("Ecosistema colapsado. Fin del mundo")
                 break
