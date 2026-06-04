d7030c87f43c002950f33dd71e66da2fe6935892
dannyoquendo07@gmail.com <dannyoquendo07@gmail.com>
2026-06-03 22:32:06 -0400

    nuevo archvo analytics creado para generar grafico con las curvas de poblacion del sistema, y importaciones a las librerias pandas y matplotlib

---
+++ analytics.py
@@ -0,0 +1,9 @@
+""" Recibi un consejo de los preparadores que en lugar de ver muñequitos moviéndose, 
+transformara la simulación en un laboratorio que muestra graficos con las curvas de poblacion. Esto usando
+la libreria pandas y matplotlib. Referencias: - https://pandas.pydata.org/docs/reference/index.html 
+- https://www.tutorialesprogramacionya.com/cienciadedatos/matplotlib/tema9.html"""
+
+import pandas as pd
+import matplotlib.pyplot as plt
+from main import SimuladorEcosistema
+from settings import ecosistema_bosque,conejos,zorros
